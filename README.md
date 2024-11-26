f1f49c1c23d5dfe45c4068627cebc9b93bafc64e
williamsjl <williamsjl@msoe.edu>
2024-11-25 23:20:08 -0600

    Lab 12 completed

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ a.  This program generates 50 frames of Mandelbrot fractal images using multipro
     child process generates a subset of the frames, ensuring efficient use of system resources.
 
 
-b. If you right click and press copy and then paste into google you can see it
+b.  If you right click and press copy and then paste into google you can see it
 
-https://msoe365-my.sharepoint.com/:i:/r/personal/williamsjl_msoe_edu/Documents/Pictures/Screenshots/Screenshot%202024-11-19%20230319.png?csf=1&web=1&e=gYidei
+    https://msoe365-my.sharepoint.com/:i:/r/personal/williamsjl_msoe_edu/Documents/Pictures/Screenshots/Screenshot%202024-11-19%20230319.png?csf=1&web=1&e=gYidei
 
 c.  This implementation shows how using multiple processes can improve the runtime for generating Mandelbrot 
     fractal images. As the number of processes increases, the workload is divided among the processes, which 
@@ -21,3 +21,19 @@ c.  This implementation shows how using multiple processes can improve the runti
     Overall, the program worked well, and the frames were generated correctly. Measuring the runtime helped 
     show how the number of processes affects performance, and the results support the idea that multiprocessing 
     can speed up intensive tasks like this one.
+
+Lab 12
+
+a.  Multithreading was used to break up the work of rendering a single image into smaller sections (regions), so multiple 
+    threads could work on these parts at the same time. This makes it faster to generate one frame by taking advantage 
+    of multiple CPU cores
+
+b. 
+
+    https://msoe365-my.sharepoint.com/:i:/r/personal/williamsjl_msoe_edu/Documents/Pictures/Screenshots/Screenshot%202024-11-25%20231116.png?csf=1&web=1&e=BiEmQH
+
+c.  Multithreading likely did better than multiprocessing because threads are faster to create and manage, with less overhead. 
+    Since they share memory within the same process, there’s no need for duplication, and dividing the image into smaller regions 
+    allows for better workload distribution. Threads also make better use of the CPU cache, making them ideal for tasks like rendering Mandelbrot set frames.
+    
+    Optimal Run Time happened with 10 threads and 10 processes or 5 processes and 20 threads
--- mandelMovie.c
+++ mandelMovie.c
@@ -7,24 +7,48 @@
 //  mandelMovie.c
 //  This file generates images of the Mandelbrot set with a zoom-in effect in parallel using semaphores
 
+// John Williams
+// CPE 2600 121
+// mandelMovie.c
+// This file generates images of the Mandelbrot set with a zoom-in effect using both multiprocessing and multithreading
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <pthread.h>
 #include <sys/wait.h>
 #include "jpegrw.h"
 #include <sys/time.h>
 
-void generate_frame(int frame_number, double xscale);
+#define MAX_THREADS 20 // Maximum threads allowed
 
-int main(int argc, char *argv[]) {
+typedef struct {
+    int thread_id;
+    int total_threads;
+    int frame_number;
+    double xscale;
+    unsigned int width;
+    unsigned int height;
+    double adjusted_xmin;
+    double adjusted_xmax;
+    double adjusted_ymin;
+    double adjusted_ymax;
+    imgRawImage* image;
+} ThreadArgs;
+
+void* generate_region(void* args);
+void generate_frame(int frame_number, double xscale, int num_threads);
+
+int main(int argc, char* argv[]) {
     int num_processes = 4;  // Default number of processes
+    int num_threads = 4;   // Default number of threads per process
     int total_frames = 50; // Total frames to generate
     double xscale = 4.0;   // Starting scale
     int opt;
 
     // Parse command-line arguments
-    while ((opt = getopt(argc, argv, "p:h")) != -1) {
+    while ((opt = getopt(argc, argv, "p:t:h")) != -1) {
         switch (opt) {
             case 'p':
                 num_processes = atoi(optarg);
@@ -33,12 +57,20 @@ int main(int argc, char *argv[]) {
                     exit(EXIT_FAILURE);
                 }
                 break;
+            case 't':
+                num_threads = atoi(optarg);
+                if (num_threads <= 0 || num_threads > MAX_THREADS) {
+                    fprintf(stderr, "Invalid number of threads. Must be between 1 and %d.\n", MAX_THREADS);
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'h':
-                printf("Usage: %s -p <num_processes>\n", argv[0]);
+                printf("Usage: %s -p <num_processes> -t <num_threads>\n", argv[0]);
                 printf("  -p <num_processes> : Number of child processes (default: 4).\n");
+                printf("  -t <num_threads>   : Number of threads per process (default: 4, max: %d).\n", MAX_THREADS);
                 exit(EXIT_SUCCESS);
             default:
-                fprintf(stderr, "Usage: %s -p <num_processes>\n", argv[0]);
+                fprintf(stderr, "Usage: %s -p <num_processes> -t <num_threads>\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
@@ -62,7 +94,7 @@ int main(int argc, char *argv[]) {
 
             for (int frame = start_frame; frame < end_frame; frame++) {
                 double scale = xscale * pow(0.95, frame); // Gradually zoom in
-                generate_frame(frame, scale);
+                generate_frame(frame, scale, num_threads);
             }
             exit(EXIT_SUCCESS);
         } else if (pids[i] < 0) { // Fork failed
@@ -82,12 +114,12 @@ int main(int argc, char *argv[]) {
                      (end_time.tv_usec - start_time.tv_usec) / 1e6;
 
     printf("All frames generated successfully.\n");
-    printf("Runtime with %d processes: %.6f seconds\n", num_processes, runtime);
+    printf("Runtime with %d processes and %d threads per process: %.6f seconds\n", num_processes, num_threads, runtime);
 
     return 0;
 }
 
-void generate_frame(int frame_number, double xscale) {
+void generate_frame(int frame_number, double xscale, int num_threads) {
     char filename[256];
     snprintf(filename, sizeof(filename), "mandel%d.jpg", frame_number);
 
@@ -102,8 +134,62 @@ void generate_frame(int frame_number, double xscale) {
     double adjusted_ymin = y_center - xscale;
     double adjusted_ymax = y_center + xscale;
 
-    // Generate Mandelbrot set
-    for (unsigned int y = 0; y < height; y++) {
+    // Create threads to process regions of the frame
+    pthread_t threads[num_threads];
+    ThreadArgs thread_args[num_threads];
+
+    for (int i = 0; i < num_threads; i++) {
+        thread_args[i] = (ThreadArgs){
+            .thread_id = i,
+            .total_threads = num_threads,
+            .frame_number = frame_number,
+            .xscale = xscale,
+            .width = width,
+            .height = height,
+            .adjusted_xmin = adjusted_xmin,
+            .adjusted_xmax = adjusted_xmax,
+            .adjusted_ymin = adjusted_ymin,
+            .adjusted_ymax = adjusted_ymax,
+            .image = image,
+        };
+
+        if (pthread_create(&threads[i], NULL, generate_region, &thread_args[i]) != 0) {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // Save the frame
+    if (storeJpegImageFile(image, filename) != 0) {
+        fprintf(stderr, "Error: Could not save frame %d to file %s\n", frame_number, filename);
+    } else {
+        printf("Saved frame %d to %s\n", frame_number, filename);
+    }
+
+    freeRawImage(image);
+}
+
+void* generate_region(void* args) {
+    ThreadArgs* thread_args = (ThreadArgs*)args;
+    int thread_id = thread_args->thread_id;
+    int total_threads = thread_args->total_threads;
+    unsigned int width = thread_args->width;
+    unsigned int height = thread_args->height;
+    double adjusted_xmin = thread_args->adjusted_xmin;
+    double adjusted_xmax = thread_args->adjusted_xmax;
+    double adjusted_ymin = thread_args->adjusted_ymin;
+    double adjusted_ymax = thread_args->adjusted_ymax;
+    imgRawImage* image = thread_args->image;
+
+    unsigned int y_start = (height / total_threads) * thread_id;
+    unsigned int y_end = (thread_id == total_threads - 1) ? height : y_start + (height / total_threads);
+
+    for (unsigned int y = y_start; y < y_end; y++) {
         for (unsigned int x = 0; x < width; x++) {
             double real = adjusted_xmin + (x / (double)width) * (adjusted_xmax - adjusted_xmin);
             double imag = adjusted_ymin + (y / (double)height) * (adjusted_ymax - adjusted_ymin);
@@ -123,7 +209,6 @@ void generate_frame(int frame_number, double xscale) {
                 iterations++;
             }
 
-
             // Color based on iterations
             unsigned char red = (iterations % 256);
             unsigned char green = (iterations * 2 % 256);
@@ -133,12 +218,5 @@ void generate_frame(int frame_number, double xscale) {
         }
     }
 
-    // Save the frame
-    if (storeJpegImageFile(image, filename) != 0) {
-        fprintf(stderr, "Error: Could not save frame %d to file %s\n", frame_number, filename);
-    } else {
-        printf("Saved frame %d to %s\n", frame_number, filename);
-    }
-
-    freeRawImage(image);
+    return NULL;
 }
